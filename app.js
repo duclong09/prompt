aedf94a51a0d75fa660b66bb23054a41534fae51
duclonggitnore <duclongsew@gmail.com>
2020-11-15 14:42:32 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//thông báo ra cửa số tin nhắn:
+let name = 'long'
+alert(name);
+
+//thông báo ra màn hình chờ cho phép người dùng nhập vào, để nhấn Ok/Cacel:
+let age = prompt('long bao nhieu tuổi', '');
+alert(`long ${age} tuoi`);
+
+//thông báo cho người dùng nhấn Ok/Hủy:
+let isName = confirm('ho duc long');
+alert(isName);
